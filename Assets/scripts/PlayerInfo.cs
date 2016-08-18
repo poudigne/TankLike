95781fa8e2451e69f519532b40fcb4e2059a01f3
poudigne <pierluc0414@live.ca>
2016-08-17 23:37:18 -0400

    arc

--- Assets/scripts/PlayerInfo.cs
+++ Assets/scripts/PlayerInfo.cs
@@ -3,58 +3,59 @@ using System.Collections;
 
 public class PlayerInfo : MonoBehaviour
 {
-  public int maxHealth;
-  public int health;
+    public int maxHealth;
+    public int health;
 
-  [SerializeField] private string _playerName;
-  
-  private PlayerNameLabel playerNameLabel;
-  private ChatController chatController;
+    [SerializeField]
+    private string _playerName;
 
-  #region Property
-  public string playerName
-  {
-    get { return _playerName; }
-    set
+    private PlayerNameLabel playerNameLabel;
+    private ChatController chatController;
+
+    #region Property
+    public string playerName
     {
-      _playerName = value;
-      if (playerNameLabel != null)
-        playerNameLabel.SetName(_playerName);
+        get { return _playerName; }
+        set
+        {
+            _playerName = value;
+            if (playerNameLabel != null)
+                playerNameLabel.SetName(_playerName);
+        }
     }
-  }
-  #endregion
-  
+    #endregion
 
-  #region Unity Engine
 
-  void Start()
-  {
-    
-    if (chatController == null)
-      Debug.LogError("ChatController not found. Please create one.");
-    health = maxHealth;
-    playerNameLabel = GetComponent<PlayerNameLabel>();
-    playerName = _playerName;
-    if (playerNameLabel == null)
-      Debug.LogError("No \"PlayerNameLabel\" component found. Please add one.");
-  }
-  #endregion
+    #region Unity Engine
 
-  public void DoDamage(float damageAmount, PlayerInfo attackerInfo)
-  {
-    int damageInt = (int) damageAmount;
-    health -= damageInt;
-    ChatController chatController = FindObjectOfType<ChatController>();
-    chatController.AddNewLine(string.Empty, attackerInfo.playerName + " attacked " + playerName + " for " + damageInt + " damage");
-    if (health <= 0)
+    void Start()
     {
-      Die();
-      chatController.AddNewLine(string.Empty, attackerInfo.playerName + " killed " + playerName);
+        chatController = FindObjectOfType<ChatController>();
+        if (chatController == null)
+            Debug.LogError("ChatController not found. Please create one.");
+        health = maxHealth;
+        playerNameLabel = GetComponent<PlayerNameLabel>();
+        playerName = _playerName;
+        if (playerNameLabel == null)
+            Debug.LogError("No \"PlayerNameLabel\" component found. Please add one.");
     }
-  }
+    #endregion
 
-  void Die()
-  {
-    Destroy(transform.gameObject);
-  }
+    public void DoDamage(float damageAmount, PlayerInfo attackerInfo)
+    {
+        int damageInt = (int)damageAmount;
+        health -= damageInt;
+        ChatController chatController = FindObjectOfType<ChatController>();
+        chatController.AddNewLine(string.Empty, attackerInfo.playerName + " attacked " + playerName + " for " + damageInt + " damage");
+        if (health <= 0)
+        {
+            Die();
+            chatController.AddNewLine(string.Empty, attackerInfo.playerName + " killed " + playerName);
+        }
+    }
+
+    void Die()
+    {
+        Destroy(transform.gameObject);
+    }
 }
