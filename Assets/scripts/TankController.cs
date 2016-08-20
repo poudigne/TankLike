4acce521b7f693b0dabca13e18f3957e20710efa
poudigne <pierluc0414@live.ca>
2016-08-20 01:11:27 -0400

    new tank movement ? (in progress)

--- Assets/scripts/TankController.cs
+++ Assets/scripts/TankController.cs
@@ -2,46 +2,125 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
 
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+[SelectionBase]
 public class TankController : MonoBehaviour
 {
-    public float movementSpeed = 1.0f;
     public float rotationSpeed = 1.0f;
     public float chargingSpeed = 0.01f;
 
     public bool isMyTurn = true;
     public bool hasFired = false;
 
-    [SerializeField]
-    private GameObject projectile;
-    [SerializeField]
-    private Slider powerSlider;
-    [SerializeField]
-    private Text powerLabel;
-    [SerializeField]
-    private Text angleLabel;
+    public GameObject projectile;
+    public Slider powerSlider;
+    public Text powerLabel;
+    public Text angleLabel;
+
+    public bool isCharging = false;
+
+    public float firePower; // the fire power of the shoot in %
+    public float fireAngleDeg;
+    public float nextTurnDelay = 2.0f;
+    public GameObject firedProjectile;
+
+    public CameraController cameraController;
+    public GameController gameController;
+
+    public float startAngle = 0.0f;
+    public float endAngle = 0.0f;
 
     [SerializeField]
-    private bool isCharging = false;
+    Vector2 _velocity = Vector2.zero;
 
     [SerializeField]
-    private float firePower; // the fire power of the shoot in %
-    [SerializeField]
-    private float fireAngleDeg;
-    [SerializeField]
-    private float nextTurnDelay = 2.0f;
-    private GameObject firedProjectile;
+    int _rayCount = 8;
 
-    private CameraController cameraController;
-    private GameController gameController;
-    #region Unity Engine
+
+    [SerializeField]
+    float MAXIMUM_CLIMB_ANGLE = 30f;
+
+    const float WALK_SPEED = 6f;
+    const float GRAVITY = 4f;
+    const float TERMINAL_VELOCITY = 12f;
+
+    float GroundCheckDelta
+    {
+        get { return _bounds.height + 0.5f; }
+    }
+
+    const float MARGIN = 0f;
+
+    Transform _spriteTransform;
+    BoxCollider2D _collider;
+    Rigidbody2D _rigidbody;
+
+    float _xMax = float.MaxValue;
+    float _yMax = float.MaxValue; //might not need since there is no vertical movement
+    float _yMin = float.MinValue;
+    float _xMin = float.MinValue;
+
+    [SerializeField]
+    bool _isGrounded;
+    [SerializeField]
+    bool _isOnSlope;
+
+    readonly List<RaycastHit2D> _raycastHits = new List<RaycastHit2D>();
+
+    Rect _bounds;
+
+    bool IsCanonMoving { get { return Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S); } }
+    bool IsTankMoving { get { return Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D); } }
+    bool IsCharging { get { return Input.GetKey(KeyCode.Space); } }
+    int GetCanonMovingDirection { get { return (Input.GetKey(KeyCode.W) ? -1 : 1); } }
+
+
+    Vector2 TopLeftBoundsPoint
+    {
+        get
+        {
+            return _bounds.min + Vector2.up * _bounds.height + MARGIN * (Vector2.right + Vector2.down);
+        }
+    }
+
+    Vector2 TopRightBoundsPoint
+    {
+        get
+        {
+            return _bounds.max + MARGIN * (Vector2.left + Vector2.down);
+        }
+    }
+
+    Vector2 BottomLeftBoundsPoint
+    {
+        get
+        {
+            return _bounds.min + +MARGIN * (Vector2.right + Vector2.up);
+        }
+    }
+
+    Vector2 BottomRightBoundsPoint
+    {
+        get
+        {
+            return _bounds.max + Vector2.down * _bounds.height + MARGIN * (Vector2.left + Vector2.up);
+        }
+    }
 
     void Awake()
     {
         gameController = FindObjectOfType<GameController>();
+
+        _collider = GetComponent<BoxCollider2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        _spriteTransform = GetComponentsInChildren<Transform>().First(t => t.name == "SpriteGraphics");
     }
 
-    // Use this for initialization
     void Start()
     {
         cameraController = Camera.main.GetComponent<CameraController>();
@@ -54,9 +133,9 @@ public class TankController : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        Debug.Log(isMyTurn);
         if (isMyTurn)
         {
             if (!hasFired)
@@ -67,7 +146,7 @@ public class TankController : MonoBehaviour
                 RotateCanon(GetCanonMovingDirection);
             // If we moving the tank
             if (IsTankMoving && !isCharging)
-                MoveTank(GetTankMovingDirection);
+                MoveTank();
             // If we're charging and it's my turn
             if (IsCharging)
                 ChargingWeapon();
@@ -83,8 +162,142 @@ public class TankController : MonoBehaviour
             }
         }
 
+       
     }
-    #endregion
+
+    void ApplyGravity()
+    {
+        _velocity.y += -GRAVITY * Time.fixedDeltaTime;
+        _velocity.y = Mathf.Clamp(_velocity.y, -TERMINAL_VELOCITY * Time.fixedDeltaTime, float.MaxValue);
+    }
+
+    void ApplyMovementInput()
+    {
+        float input = Input.GetAxisRaw("Horizontal");
+
+        GetRaycastInfo(TopLeftBoundsPoint, TopRightBoundsPoint, Vector2.down, 100f);
+
+        var shortestRay = _raycastHits.Where(hit => hit.collider != null)
+            .OrderBy(hit => hit.fraction)
+            .FirstOrDefault(hit => Math.Abs(hit.fraction) > 0.0001f);
+
+        float angle = Vector2.Angle(shortestRay.normal, Vector2.up);
+
+        _velocity.x = input * WALK_SPEED * Time.fixedDeltaTime * Mathf.Cos(angle * Mathf.Deg2Rad);
+    }
+
+    void UpdatePosition()
+    {
+        var newPos = new Vector2
+        {
+            x = _rigidbody.position.x + _velocity.x,
+            y = _rigidbody.position.y + _velocity.y
+        };
+
+        _rigidbody.MovePosition(newPos);
+    }
+
+    void CheckGrounded()
+    {
+        GetRaycastInfo(TopLeftBoundsPoint, TopRightBoundsPoint, Vector2.down, _bounds.height + GroundCheckDelta);
+
+        if (_raycastHits.Any(hit => hit.collider != null))
+        {
+            var shortestRay = _raycastHits.Where(hit => hit.collider != null)
+                .OrderBy(hit => hit.fraction)
+                .FirstOrDefault(hit => Math.Abs(hit.fraction) > 0.0001f);
+
+            if (shortestRay.distance < GroundCheckDelta)
+            {
+                _velocity.y = 0;
+                _isGrounded = true;
+
+                _isOnSlope = Math.Abs(Vector2.Angle(shortestRay.normal, Vector2.up)) > 0.0001f;
+
+                float targetY = shortestRay.point.y + _bounds.height / 2;
+
+                if (_isOnSlope)
+                {
+                    var startPoint = TopLeftBoundsPoint + Vector2.right * _bounds.width / 2;
+                    var middleRay = Physics2D.Raycast(startPoint, Vector2.down, _bounds.height + 0.3f);
+                    Debug.DrawLine(_bounds.center, middleRay.point, Color.green);
+
+                    if (middleRay.collider != null)
+                    {
+                        targetY = middleRay.point.y + _bounds.height / 2;
+                    }
+                }
+                _rigidbody.position = new Vector2(_rigidbody.position.x, targetY);
+            }
+            else
+            {
+                _isGrounded = false;
+                _isOnSlope = false;
+            }
+        }
+        else
+        {
+            _isGrounded = false;
+            _isOnSlope = false;
+        }
+    }
+
+    void GetRaycastInfo(Vector2 startPoint, Vector2 endPoint, Vector2 direction, float distance)
+    {
+        _raycastHits.Clear();
+
+        for (int i = 0; i < _rayCount; i++)
+        {
+            float lerpAmount = (float)i / (_rayCount - 1);
+            var raycastOrigin = Vector2.Lerp(startPoint, endPoint, lerpAmount);
+
+            var hitInfo = Physics2D.Raycast(raycastOrigin, direction, distance);
+
+            _raycastHits.Add(hitInfo);
+            if (hitInfo.collider != null)
+            {
+                Debug.DrawLine(raycastOrigin, hitInfo.point, Color.cyan);
+            }
+
+        }
+    }
+
+    void AlignSpriteToGround()
+    {
+        //TODO: get sprite transform width/height?
+        Vector2 startPoint = _spriteTransform.position - _spriteTransform.right * 0.5f - _spriteTransform.up * 0.5f;
+        startPoint.y += MARGIN;
+        startPoint.x += MARGIN;
+
+        Vector2 endPoint = _spriteTransform.position + _spriteTransform.right * 0.5f - _spriteTransform.up * 0.5f;
+        endPoint.y += MARGIN;
+        endPoint.x -= MARGIN;
+
+        var hitInfoStart = Physics2D.Raycast(startPoint, Vector2.down, 4f);
+        var hitInfoEnd = Physics2D.Raycast(endPoint, Vector2.down, 4f);
+
+        var hitInfos = new[] { hitInfoStart, hitInfoEnd };
+
+        int hitCount = hitInfos.Count(h => h.collider != null);
+
+        //Align to ground
+        switch (hitCount)
+        {
+            case 2:
+                var alignAxis = hitInfoEnd.point - hitInfoStart.point;
+                _spriteTransform.up = Vector3.Cross(alignAxis, Vector3.back).normalized;
+                break;
+            case 1:
+                var hitInfo = hitInfos.First(h => h.collider != null);
+                _spriteTransform.up = hitInfo.normal;
+                break;
+            default:
+                _spriteTransform.up = Vector2.up;
+                break;
+        }
+
+    }
+
     // Find the UI element for future reference, this is done programaticaly because tank are Created programmaticaly
     void InitilializeUIElement()
     {
@@ -98,28 +311,37 @@ public class TankController : MonoBehaviour
     {
         var canonTransform = GetCanon();
 
-        var modifier = rotationSpeed * direction;
-        canonTransform.Rotate(Vector3.forward * modifier);
+        if (direction == -1)
+            canonTransform.Rotate(Vector3.back);
+        else
+            canonTransform.Rotate(Vector3.forward);
+
         fireAngleDeg = canonTransform.eulerAngles.z;
         RestrictCanonRotation(canonTransform);
-
         UpdateFireAngleUI();
     }
 
     // We move the tank in the direction in the parameter : -1 to the left; 1 to the right
-    void MoveTank(int direction)
+    void MoveTank()
     {
-        var canonTransform = GetCanon();
-        if (transform.localScale.x != direction)
-        {
-            gameObject.transform.localScale = new Vector3(direction, transform.localScale.y);
-            Debug.Log("Changing canon directions");
-        }
-        
-        fireAngleDeg = (transform.localScale.x < 0 ? 360 - canonTransform.eulerAngles.z : canonTransform.eulerAngles.z);
+        _bounds.x = _collider.bounds.min.x;
+        _bounds.y = _collider.bounds.min.y;
+        _bounds.width = _collider.bounds.size.x;
+        _bounds.height = _collider.bounds.size.y;
 
-        UpdateFireAngleUI();
-        transform.GetComponent<Rigidbody2D>().velocity = new Vector2(movementSpeed * direction, transform.GetComponent<Rigidbody2D>().velocity.y);
+        //_velocity.x = _velocity.y = 0f;
+        ApplyMovementInput();
+
+        if (_isGrounded == false)
+        {
+            ApplyGravity();
+        }
+
+        CheckGrounded();
+
+        AlignSpriteToGround();
+
+        UpdatePosition();
     }
 
     // if we're holding space bar, we're charging the attack
@@ -143,7 +365,7 @@ public class TankController : MonoBehaviour
         firedProjectile = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
         ProjectileController projectileController = firedProjectile.GetComponent<ProjectileController>();
         PlayerInfo attackerInfo = GetComponent<PlayerInfo>();
-        
+
         projectileController.FireProjectile(firePower, fireAngleDeg, attackerInfo);
         firePower = 0.0f;
         hasFired = true;
@@ -152,10 +374,11 @@ public class TankController : MonoBehaviour
     // Restrict the canon rotation
     void RestrictCanonRotation(Transform canonTransform)
     {
-        if (fireAngleDeg > 90 && fireAngleDeg < 270)
-            canonTransform.eulerAngles = new Vector3(canonTransform.eulerAngles.x, canonTransform.eulerAngles.y, 90);
-        else if (fireAngleDeg < 270 && fireAngleDeg > 90)
-            canonTransform.eulerAngles = new Vector3(canonTransform.eulerAngles.x, canonTransform.eulerAngles.y, 270);
+        canonTransform.eulerAngles = new Vector3(canonTransform.eulerAngles.x, canonTransform.eulerAngles.y, Mathf.Clamp(fireAngleDeg, startAngle, endAngle));
+        //if (fireAngleDeg > startAngle && fireAngleDeg < endAngle)
+        //    canonTransform.eulerAngles = new Vector3(canonTransform.eulerAngles.x, canonTransform.eulerAngles.y, 90);
+        //else if (fireAngleDeg < 270 && fireAngleDeg > 90)
+        //    canonTransform.eulerAngles = new Vector3(canonTransform.eulerAngles.x, canonTransform.eulerAngles.y, 270);
     }
 
     Text FindUILabelWithName(string labelname)
@@ -185,15 +408,6 @@ public class TankController : MonoBehaviour
         gameController.PlayNext();
     }
 
-    #region UI Update
-    // Attach the UI ELements to the GameObject
-    public void HookUIElements()
-    {
-        powerSlider = GameObject.FindGameObjectWithTag("power_value_slider").GetComponent<Slider>();
-        powerLabel = GameObject.FindGameObjectWithTag("power_value_label").GetComponent<Text>();
-        angleLabel = GameObject.FindGameObjectWithTag("angle_value_label").GetComponent<Text>();
-    }
-
     // Update the UI element related to power (Slider and Label)
     void UpdateFirePowerUI()
     {
@@ -207,22 +421,13 @@ public class TankController : MonoBehaviour
     {
         angleLabel.text = ((int)fireAngleDeg).ToString();
     }
-    #endregion
-    #region Input check
-    // if we're rotating the canon left or right
-    bool IsCanonMoving { get { return Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S); } }
-    // If we're moving tank left or Right
-    bool IsTankMoving { get { return Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D); } }
-    // If we're holding space key to charge up the fire
-    bool IsCharging { get { return Input.GetKey(KeyCode.Space); } }
+    
 
-    // If we press w we want to rotate to the left so we return -1 otherwise 1
-    int GetCanonMovingDirection { get { return (Input.GetKey(KeyCode.W) ? -1 : 1); } }
-    // If we press A we want to move to the left so we return -1 otherwise 1
-    int GetTankMovingDirection { get { return (Input.GetKey(KeyCode.A) ? -1 : 1); } }
-    #endregion
-    #region
-
-
-    #endregion
+    // Attach the UI ELements to the GameObject
+    public void HookUIElements()
+    {
+        powerSlider = GameObject.FindGameObjectWithTag("power_value_slider").GetComponent<Slider>();
+        powerLabel = GameObject.FindGameObjectWithTag("power_value_label").GetComponent<Text>();
+        angleLabel = GameObject.FindGameObjectWithTag("angle_value_label").GetComponent<Text>();
+    }
 }
